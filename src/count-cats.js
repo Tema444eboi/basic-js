8b487d6b213ddf5d050b4e1573c9f6c727626e57
Tema444eboi <137617703+Tema444eboi@users.noreply.github.com>
2023-12-21 19:51:02 +0100

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,15 +1,31 @@
+const { NotImplementedError } = require('../extensions/index.js');
 
-function countCats(matrix) {
-  let number = 0
-  for (let i = 0; i < matrix.length; i++) {
-    for (let j = 0; j < matrix[i].length; j++) {
-      if (matrix[i][j] == '^^') {
-        number++
+/**
+ * Given matrix where you have to find cats by ears "^^"
+ *
+ * @param {Array<Array>} matrix 
+ * @return {Number} count of cats found
+ *
+ * @example
+ * countCats([
+ *  [0, 1, '^^'],
+ *  [0, '^^', 2],
+ *  ['^^', 1, 2]
+ * ]) => 3`
+ *
+ */
+ function countCats(matrix) {
+  let counter = 0;
+  matrix.forEach((yard) => {
+    yard.forEach((yardItem) => {
+      if (yardItem === "^^") {
+        counter++;
       }
-    }
-  }
-  return number
+    });
+  });
+  return counter ? counter : 0;
 }
+
 module.exports = {
   countCats
 };
